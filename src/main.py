caa2e271c7d97fd8627663fc097ec26c0dc9b6c7
jlouange <j.mushumba@alustudent.com>
2026-09-06 17:43:46 +0200

    Complete working data extraction

--- src/main.py
+++ src/main.py
@@ -1,69 +1,95 @@
 import re
 import json
 
-# emails
+
 
 with open ("input/raw-text.txt", "r") as file:
-    data = file.readlines()
+    raw_lines = file.readlines()
+
+json_data = {
+    "emails": [],
+    "phone_numbers": [],
+    "credit_cards": [],
+    "urls": []
+}
+# emails
 
 email_pattern = r"([a-zA-Z0-9](?:[a-zA-Z0-9._]*[a-zA-Z0-9])?)@((?:alueducation|alumni\.alueducation|si\.alueducation)\.com)(?![.\w<])"
 emails = []
-for line in data:
+for line in raw_lines:
     clean_line = line.strip()  # Removes trailing \n and spaces
     if not clean_line:
         continue
-    match = re.search(email_pattern, clean_line)
+    matches = re.finditer(email_pattern, clean_line)
 
-    if match:
-        username = match.group(1)
-        domain = match.group(2)
-        extracted_email = username + "@" + domain
+    for match in matches:
+        extracted_email = match.group(0)
         emails.append(extracted_email)
-with open("output/sample-output.json", "r") as file:
-    data1 = json.load(file)
-for email in emails:
-    data1["emails"].append(email)
 
-with open("output/sample-output.json", "w") as file:
-    json.dump(data1, file, indent=4)
+
 
 # phone number
 
-phone_pattern = r"(?:(?:\+?250[\s-]?)?(\d{3}[ -]\d{3}[ -]\d{3}))|(0\d{9})"
+phone_pattern = r"(?<!\d)(?:\+250[\s-]?\d{3}[\s-]?\d{3}[\s-]?\d{3}|0\d{9})(?!\d)"
 phone_numbers = []
-for line in data:
+for line in raw_lines:
     clean_line = line.strip()  
     if not clean_line:
         continue
-    match = re.search(phone_pattern, clean_line)
-    if match:
+    matches = re.finditer(phone_pattern, clean_line)
+
+    for match in matches:
         phone_number = match.group(0)
         phone_numbers.append(phone_number)
 
-with open("output/sample-output.json", "r") as file:
-    data1 = json.load(file)
-for phone_number in phone_numbers:
-    data1["phone_numbers"].append(phone_number)
 
-with open("output/sample-output.json", "w") as file:
-    json.dump(data1, file, indent=4)
 
+# credit card number
 
 credit_pattern = r"(\d{4}[ -]?\d{4}[ -]?\d{4}[ -]?\d{4})(?![-\w])"
 credit_card_numbers = []
-for line in data:
+for line in raw_lines:
     clean_line = line.strip()  
     if not clean_line:
         continue
-    match = re.search(credit_pattern, clean_line)
-    if match:
+    matches = re.finditer(credit_pattern, clean_line)
+
+    for match in matches:
         credit_card_number = match.group(0)
         credit_card_numbers.append(credit_card_number)
 
-with open("output/sample-output.json", "r") as file:
-    data1 = json.load(file)
-for credit_card_number in credit_card_numbers:
-    data1["credit_cards"].append(credit_card_number)
+
+
+# URLs
+
+url_pattern = r"https?:\/\/(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(?:\/[a-zA-Z0-9](?:[a-zA-Z0-9._-]*[a-zA-Z0-9])?)*(?![a-zA-Z0-9._/-])"
+urls_addresses = []
+for line in raw_lines:
+    clean_line = line.strip()  
+    if not clean_line:
+        continue
+    matches = re.finditer(url_pattern, clean_line)
+
+    for match in matches:
+        url_address = match.group(0)
+        urls_addresses.append(url_address)
+
+print("EMAILS:", len(emails))
+print(emails)
+
+print("PHONES:", len(phone_numbers))
+print(phone_numbers)
+
+print("CARDS:", len(credit_card_numbers))
+print(credit_card_numbers)
+
+print("URLS:", len(urls_addresses))
+print(urls_addresses)
+
+json_data["emails"] = emails
+json_data["phone_numbers"] = phone_numbers
+json_data["credit_cards"] = credit_card_numbers
+json_data["urls"] = urls_addresses
 
 with open("output/sample-output.json", "w") as file:
-    json.dump(data1, file, indent=4)
+    json.dump(json_data, file, indent=4)
