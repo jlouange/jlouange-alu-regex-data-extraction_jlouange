6960763a79059a5d72bd1bbb56f86911fde84701
jlouange <j.mushumba@alustudent.com>
2026-09-06 03:53:52 +0200

    Phone number filtering

--- src/main.py
+++ src/main.py
@@ -26,3 +26,24 @@ for email in emails:
 
 with open("output/sample-output.json", "w") as file:
     json.dump(data1, file, indent=4)
+
+# phone number
+
+phone_pattern = r"(?:(?:\+?250[\s-]?)?(\d{3}[ -]\d{3}[ -]\d{3}))|(0\d{9})"
+phone_numbers = []
+for line in data:
+    clean_line = line.strip()  
+    if not clean_line:
+        continue
+    match = re.search(phone_pattern, clean_line)
+    if match:
+        phone_number = match.group(0)
+        phone_numbers.append(phone_number)
+
+with open("output/sample-output.json", "r") as file:
+    data1 = json.load(file)
+for phone_number in phone_numbers:
+    data1["phone_numbers"].append(phone_number)
+
+with open("output/sample-output.json", "w") as file:
+    json.dump(data1, file, indent=4)
